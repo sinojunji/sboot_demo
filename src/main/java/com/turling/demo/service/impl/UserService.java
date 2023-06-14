3b1a1a5613b872ddaaa9e44eab77123bba5efc4d
george.w <sinojunji@gmail.com>
2023-06-14 17:16:00 +0800

    sync

--- src/main/java/com/turling/demo/service/impl/UserService.java
+++ src/main/java/com/turling/demo/service/impl/UserService.java
@@ -1,18 +1,17 @@
 package com.turling.demo.service.impl;
 
+
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.turling.demo.entity.User;
 import com.turling.demo.mapper.UserMapper;
-import com.turling.demo.service.IBaseService;
 import com.turling.demo.service.IUserService;
 import com.turling.demo.utils.EntityUtils;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
+
 import javax.annotation.Resource;
-import java.lang.reflect.Type;
 import java.util.List;
 
 @Service
